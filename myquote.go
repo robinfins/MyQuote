d86a4280befdf35f6bf4a35152f20996c101ab36
Robin <89386472+robinfins@users.noreply.github.com>
2022-01-27 21:06:21 +0100

    Update myquote.go

--- myquote.go
+++ myquote.go
@@ -6,9 +6,18 @@ import (
 	"rsc.io/quote"
 )
 
-func main() {
+func Glass() {
 	fmt.Println(quote.Glass())
+}
+
+func Go() {
 	fmt.Println(quote.Go())
+}
+
+func Hello() {
 	fmt.Println(quote.Hello())
+}
+
+func Opt() {
 	fmt.Println(quote.Opt())
 }
